f48bd72523075eb8f2fadd345e98893c6a0d45f8
bubobou <bruno.bourgine@gmail.com>
2022-04-03 18:59:45 +0200

    ajout modules pour release
    	supprimé :        app/interface.py
    	nouveau fichier : app/release/calcul_coordonnees_tatamis.py
    	nouveau fichier : app/release/dojo.py
    
    	nouveau fichier : app/release/calcul_coordonnees_tatamis.py
    	nouveau fichier : app/release/dojo.py

---
+++ app/release/calcul_coordonnees_tatamis.py
@@ -0,0 +1,96 @@
+import sys
+import time
+import datetime
+import numpy as np
+
+class Dispositions:
+    "classe qui calcul les coordonnées des tatamis d'après les dimensions du dojo"
+
+    def __init__(self, H, W):
+        if H < W :
+            self.H = H
+            self.W = W
+        else :
+            self.H = W
+            self.W = H
+      
+        self.count = 0 
+        self.solutions=[]
+        self.room=self.init_room()        
+                
+        self.coordonnees=self.listeTatamis()
+
+    def init_room(self):
+        "fonction qui créé une matrice de 0 de dimension H*W"
+        room = np.zeros((self.H+2, self.W+2))
+        room[0,:] = -1
+        room[self.H+1,:] = -1
+        room[:,0] = -1
+        room[:,self.W+1] = -1
+        return room
+
+    def setTatami_rowscan(self,h,w,idx)->int:
+        '''
+        Paramètres
+        ----------
+        (h,w) : La position courante d'exploration du dojo. h correspond au numéro de la ligne,
+        w correspond au numéro de la colonne.
+        idx   : le numéro d'identification du Tatami en courant de positionnement.
+
+        Incrémentation: 
+            Le nombre total de dispositions possibles (count :int).
+            La liste contenant toutes les dispositions  (solutions : list)
+
+        '''        
+               
+        if   h == self.H + 1:
+            self.count = self.count + 1        
+            self.solutions.append(self.room.copy())
+            
+        elif w == self.W + 1: 
+            # Reach the right boundary, go to explore the next row from the left 
+            self.setTatami_rowscan(h+1, 1, idx)
+        elif self.room[h,w] > 0: 
+            # This grid has been occupied, move to the right one
+            self.setTatami_rowscan(h, w+1, idx)
+        elif self.room[h-1,w]==self.room[h-1,w-1] or self.room[h,w-1]==self.room[h-1,w-1]:
+            # if (the same IDX for up and left-up) or (the same IDX for left and left-up), 
+            # Tatami arrangement is allowed.
+            if self.room[h,w+1]==0: 
+                # Horizontal arrangement is allowed
+                self.room[h,w]   = idx
+                self.room[h,w+1] = idx
+                        
+                self.setTatami_rowscan(h, w+2, idx+1)            
+                self.room[h,w]   = 0
+                self.room[h,w+1] = 0  
+            if self.room[h+1,w]==0:
+                # Vertical arrangement is allowed
+                self.room[h,w]   = idx
+                self.room[h+1,w] = idx            
+                self.setTatami_rowscan(h, w+1, idx+1)        
+                self.room[h,w]   = 0
+                self.room[h+1,w] = 0
+
+    
+    def listeTatamis(self):
+        ''' fonction qui renvoie une liste de tatamis décrits par un dictionnaire contenant leur largeur, hauteur et position'''
+        self.setTatami_rowscan(1,1,1) 
+        dispositions=[]
+        for array in self.solutions :
+            tatamis = []    
+            for i in range(1,self.H+1):
+                for j in range(1, self.W+1):
+                    if array[i,j]==array[i,j+1]:
+                        tatamis.append(dict(id=int(array[i,j]),x=j-1,y=i-1,largeur=2,hauteur=1))
+                    elif array[i,j]==array[i+1,j]:
+                        tatamis.append(dict(id=int(array[i,j]),x=j-1,y=i-1,largeur=1,hauteur=2))
+            dispositions.append(tatamis)
+        return dispositions
+    
+# décommenter les lignes pour tester la classe Disposition
+
+# tStart = time.perf_counter()
+# disp=Dispositions(20,19)
+# tCost  = time.perf_counter() - tStart
+# print('count = {0}, tCost = {1:6.3f}(sec)'.format(disp.count,tCost))
---
+++ app/release/dojo.py
@@ -0,0 +1,99 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QLineEdit, QGridLayout, QMessageBox, QDialog, QVBoxLayout, QDialogButtonBox
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsRectItem, QGraphicsItem
+from PyQt5.QtGui import QBrush, QColor, QPen, QPainter
+from calcul_coordonnees_tatamis import Dispositions
+
+
+COULEUR_FOND = "white"
+COULEUR_CONTOUR = "gray"
+COULEUR_TATAMI = "yellow"
+
+class Tatami(QGraphicsRectItem):
+    "classe permettant d'instancier un tatamis d'après ces propriétés: position,dimensions,couleur"
+
+    _pen = QPen(QColor(COULEUR_CONTOUR))
+    _pen.setCosmetic(True)
+
+    def __init__(self,x,y,l,h):
+        QGraphicsRectItem.__init__(self,0,0,l,h)
+        self.setPos(x,y)
+        self.setPen(Tatami._pen)
+        self.brush = QBrush(QColor(COULEUR_TATAMI))
+        self.setBrush(self.brush)
+
+class Dojos(QGraphicsScene):
+    "classe créant un seul ou tous les dojos possibles d'après les dimensions données"
+    
+    def __init__(self,H,W,tous=False):
+        QGraphicsScene.__init__(self)
+
+        self.H = H
+        self.W = W
+        self.tous = tous
+        self.ajoutDojos()
+
+    def ajoutDojos(self):
+        "fonction permettant d'ajouter un dojo sur la scène"        
+        placementTatamis = Dispositions(self.W,self.H)
+        if self.tous :
+            dojos = placementTatamis.coordonnees
+        else :
+            dojos = [placementTatamis.coordonnees[0]]
+
+        decalage = 0
+        size = min(750//self.W,550//self.H)
+        for dojo in dojos:
+            self.ajoutTatamis(dojo,decalage,size)
+            decalage += (self.W)*size+50
+
+    def ajoutTatamis(self,dojo,decalage,size):
+        "fonction plaçant un tatamis sur la scene d'après ses propriétés"
+        for element in dojo:
+                tatami = Tatami(element['x']*size,decalage+element['y']*size,element['largeur']*size,element['hauteur']*size)
+                self.addItem(tatami)
+
+class VueDojo(QGraphicsView):
+    "classe permettant d'instancier la vue contenant les dojos"
+    
+    def __init__(self,dojo):
+        QGraphicsView.__init__(self,dojo)
+        self.setBackgroundBrush(QColor(COULEUR_FOND))
+        #self.resize (800,600)
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     dojos = Dojos(7,12,True)
+#     vueDojo = VueDojo(dojos)
+#     vueDojo.show()
+#     sys.exit(app.exec_())
+
+
+
+class FenetreDojos(QDialog):
+    "classe qui crée la fenêtre d'affichage du ou des dojos"
+    def __init__(self,H,W,tous):
+        super().__init__()
+        self.H = H
+        self.W = W
+        self.tous = tous
+
+        self.setWindowTitle("Dispositions")
+
+        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        
+        dojos = Dojos(self.H,self.W,tous)
+        
+        vueDojo = VueDojo(dojos)
+
+        self.layout.addWidget(vueDojo)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+        self.resize (800,600)
