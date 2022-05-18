e89f6f9584e6c9707bca1ea12a787ccd33b753dc
bubobou <bruno.bourgine@gmail.com>
2022-05-18 21:04:05 +0200

    init test_prod

---
+++ app/prod/test_prod.py
@@ -0,0 +1,6 @@
+import calcul_coordonnees_tatamis
+import prod
+
+def test_nombre_dispositions_uniques():
+"""Test du calcul du nombre de dispositions uniques, excluant les symétries verticales et horizontales"""
+    return
